a31ab285e6922d311a743c42807c16e89716a278
mfirliismail <m.firliismail@gmail.com>
2025-03-18 14:51:10 +0700

    update

--- bun-mfe2-producer/src/App.tsx
+++ bun-mfe2-producer/src/App.tsx
@@ -1,10 +1,16 @@
 import './index.css';
+import Button from './button';
+import Card from "./card"
 
 const App = () => {
   return (
     <div className="content">
       <h1>Rsbuild with React</h1>
       <p>Start building amazing things with Rsbuild.</p>
+      <Button title='test'></Button>
+      <br />
+      <br />
+      <Card></Card>
     </div>
   );
 };
